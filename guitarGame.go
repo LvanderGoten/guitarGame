ff526b126b3e7d88a9d93c954d975c8cbef82320
Lennart Van der Goten <lvandergoten@gmail.com>
2021-12-03 12:27:19 +0100

    lin.alg.operations

--- guitarGame.go
+++ guitarGame.go
@@ -8,17 +8,17 @@ import (
 )
 
 const (
-	NumStrings int = 6
-	NumFrets int = 25
-	MinOctave int = 2
-	MaxOctave int = 6
-	CylinderRadius float64 = 1.0
-	CylinderHeight float64 = 5.0
-	CylinderNumRotationAngles int = 32
-	CylinderNumHeightDivisions int = 100
-	ScreenWidth int = 512
-	ScreenHeight int = 512
-	DistanceToCameraPlane float64 = 20.0
+	NumStrings                 int     = 6
+	NumFrets                   int     = 25
+	MinOctave                  int     = 2
+	MaxOctave                  int     = 6
+	CylinderRadius             float64 = 1.0
+	CylinderHeight             float64 = 5.0
+	CylinderNumRotationAngles  int     = 32
+	CylinderNumHeightDivisions int     = 100
+	ScreenWidth                int     = 512
+	ScreenHeight               int     = 512
+	DistanceToCameraPlane      float64 = 20.0
 )
 
 func getOpenStringNotes() [6]string {
@@ -38,14 +38,14 @@ func getCanonicalNotes() [12]string {
 
 func getIntrinsicMatrix() [3][4]float64 {
 	return [3][4]float64{
-		{0, 1, 2, 3} ,
-		{4, 5, 6, 7} ,
-		{8, 9, 10, 11}
+		{DistanceToCameraPlane, 0.0, float64(ScreenWidth) / 2.0, 0.0},
+		{0.0, DistanceToCameraPlane, float64(ScreenHeight) / 2.0, 0.0},
+		{0.0, 0.0, 1.0, 0.0},
 	}
 }
 
 type Vertex struct {
-	id int
+	id    int
 	coord Vector3d
 }
 
@@ -65,12 +65,12 @@ type Vector3d struct {
 
 type Cylinder struct {
 	vertices []Vertex
-	faces []Face
+	faces    []Face
 }
 
 type Camera struct {
 	position Vector3d
-	lookAt Vector3d
+	lookAt   Vector3d
 }
 
 func plus(v1 Vector3d, v2 Vector3d) Vector3d {
@@ -82,7 +82,7 @@ func minus(v1 Vector3d, v2 Vector3d) Vector3d {
 }
 
 func dotProduct(v1 Vector3d, v2 Vector3d) float64 {
-	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
+	return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z
 }
 
 func scaleByScalar(v Vector3d, s float64) Vector3d {
@@ -91,17 +91,17 @@ func scaleByScalar(v Vector3d, s float64) Vector3d {
 
 func crossProduct(v1 Vector3d, v2 Vector3d) Vector3d {
 	return Vector3d{
-		v1.y * v2.z - v1.z * v2.y,
-		v1.z * v2.x - v1.x * v2.z,
-		v1.x * v2.y - v1.y * v2.x,
+		v1.y*v2.z - v1.z*v2.y,
+		v1.z*v2.x - v1.x*v2.z,
+		v1.x*v2.y - v1.y*v2.x,
 	}
 }
 
 func euclideanNorm(v Vector3d) float64 {
-	return math.Sqrt(v.x * v.x + v.y * v.y + v.z * v.z)
+	return math.Sqrt(v.x*v.x + v.y*v.y + v.z*v.z)
 }
 
-func matrixVectorProduct(A [][]float64, b[]float64) []float64 {
+func matrixVectorProduct(A [][]float64, b []float64) []float64 {
 	if len(A[0]) != len(b) {
 		panic("Matrix-vector product is ill-defined!")
 	}
@@ -131,7 +131,7 @@ func matrixMatrixProduct(A [][]float64, B [][]float64) [][]float64 {
 
 	for i := 0; i < m; i++ {
 		result[i] = make([]float64, p)
-		for j := 0; j < p ; j++ {
+		for j := 0; j < p; j++ {
 			for k := 0; k < n; k++ {
 				result[i][j] += A[i][k] * B[k][j]
 			}
@@ -151,7 +151,7 @@ func computeMeanCoordinate(cylinder *Cylinder) Vector3d {
 	meanCoord := Vector3d{0.0, 0.0, 0.0}
 	for i, vertex := range cylinder.vertices {
 		i := float64(i)
-		meanCoord = plus(scaleByScalar(meanCoord, i/(i + 1)), scaleByScalar(vertex.coord, 1/(i + 1)))
+		meanCoord = plus(scaleByScalar(meanCoord, i/(i+1)), scaleByScalar(vertex.coord, 1/(i+1)))
 	}
 	return meanCoord
 }
@@ -160,10 +160,10 @@ func getCylinder(x0 float64, y0 float64) *Cylinder {
 	var cylinder *Cylinder
 	cylinder = new(Cylinder)
 
-	zInc := CylinderHeight / float64(CylinderNumHeightDivisions - 1)
-	phiInc := (2.0 * math.Pi) / float64(CylinderNumRotationAngles - 1)
+	zInc := CylinderHeight / float64(CylinderNumHeightDivisions-1)
+	phiInc := (2.0 * math.Pi) / float64(CylinderNumRotationAngles-1)
 
-	cylinder.vertices = make([]Vertex, CylinderNumHeightDivisions * CylinderNumRotationAngles + 2)
+	cylinder.vertices = make([]Vertex, CylinderNumHeightDivisions*CylinderNumRotationAngles+2)
 
 	// Vertices
 	for i := 0; i < CylinderNumHeightDivisions; i++ {
@@ -172,23 +172,23 @@ func getCylinder(x0 float64, y0 float64) *Cylinder {
 		for j := 0; j < CylinderNumRotationAngles; j++ {
 			phi := float64(j) * phiInc
 
-			x := x0 + CylinderRadius * math.Cos(phi)
-			y := y0 + CylinderRadius * math.Sin(phi)
+			x := x0 + CylinderRadius*math.Cos(phi)
+			y := y0 + CylinderRadius*math.Sin(phi)
 			z := z
 
-			cylinder.vertices[i * CylinderNumRotationAngles + j] = Vertex{i * CylinderNumRotationAngles + j, Vector3d{x, y, z}}
+			cylinder.vertices[i*CylinderNumRotationAngles+j] = Vertex{i*CylinderNumRotationAngles + j, Vector3d{x, y, z}}
 		}
 	}
 
 	// Faces (triangulation)
 	cylinder.faces = make([]Face, 0)
-	for i := 0; i < CylinderNumHeightDivisions - 1; i++ {
+	for i := 0; i < CylinderNumHeightDivisions-1; i++ {
 		for j := 0; j < CylinderNumRotationAngles; j++ {
 
-			v1i := i * CylinderNumRotationAngles + j
-			v2i := v1i - v1i % CylinderNumRotationAngles + (v1i + 1) % CylinderNumRotationAngles
+			v1i := i*CylinderNumRotationAngles + j
+			v2i := v1i - v1i%CylinderNumRotationAngles + (v1i+1)%CylinderNumRotationAngles
 			v3i := v1i + CylinderNumRotationAngles
-			v4i := v3i - v3i % CylinderNumRotationAngles + (v3i + 1) % CylinderNumRotationAngles
+			v4i := v3i - v3i%CylinderNumRotationAngles + (v3i+1)%CylinderNumRotationAngles
 
 			v1 := &cylinder.vertices[v1i]
 			v2 := &cylinder.vertices[v2i]
@@ -204,8 +204,8 @@ func getCylinder(x0 float64, y0 float64) *Cylinder {
 	}
 
 	// Faces bottom (triangulation)
-	vcbi := (CylinderNumHeightDivisions - 1) * CylinderNumRotationAngles + CylinderNumRotationAngles
-	vcb := Vertex{ id: vcbi, coord: Vector3d{x0, y0, 0}}
+	vcbi := (CylinderNumHeightDivisions-1)*CylinderNumRotationAngles + CylinderNumRotationAngles
+	vcb := Vertex{id: vcbi, coord: Vector3d{x0, y0, 0}}
 	cylinder.vertices[vcbi] = vcb
 	for j := 0; j < CylinderNumRotationAngles; j++ {
 		v1i := j
@@ -221,12 +221,12 @@ func getCylinder(x0 float64, y0 float64) *Cylinder {
 	}
 
 	// Faces top (triangulation)
-	vcti := (CylinderNumHeightDivisions - 1) * CylinderNumRotationAngles + CylinderNumRotationAngles + 1
-	vct := Vertex{ id: vcti, coord: Vector3d{x0, y0, CylinderHeight}}
+	vcti := (CylinderNumHeightDivisions-1)*CylinderNumRotationAngles + CylinderNumRotationAngles + 1
+	vct := Vertex{id: vcti, coord: Vector3d{x0, y0, CylinderHeight}}
 	cylinder.vertices[vcti] = vct
 	for j := 0; j < CylinderNumRotationAngles; j++ {
-		v1i := (CylinderNumHeightDivisions - 1) * CylinderNumRotationAngles + j
-		v2i := v1i - v1i % CylinderNumRotationAngles + (v1i + 1) % CylinderNumRotationAngles
+		v1i := (CylinderNumHeightDivisions-1)*CylinderNumRotationAngles + j
+		v2i := v1i - v1i%CylinderNumRotationAngles + (v1i+1)%CylinderNumRotationAngles
 
 		v1 := &cylinder.vertices[v1i]
 		v2 := &cylinder.vertices[v2i]
@@ -267,7 +267,7 @@ func writeToObjFile(cylinder *Cylinder, fileName string) {
 	}
 
 	for _, face := range cylinder.faces {
-		_, err := f.WriteString(fmt.Sprintf("f %d %d %d\n", face.v1.id + 1, face.v2.id + 1, face.v3.id + 1))
+		_, err := f.WriteString(fmt.Sprintf("f %d %d %d\n", face.v1.id+1, face.v2.id+1, face.v3.id+1))
 		if err != nil {
 			return
 		}
